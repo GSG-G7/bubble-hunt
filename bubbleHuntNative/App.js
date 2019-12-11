a3b8daf2cdf490a76aece387b32498f3ae6a5782
fadeomar <fadeomar2015@gmail.com>
2019-12-11 12:47:28 +0200

    install pk for navagations

--- bubbleHuntNative/App.js
+++ bubbleHuntNative/App.js
@@ -1,13 +1,33 @@
 import React from 'react';
-import {SafeAreaView, StyleSheet, ScrollView, View, Text, StatusBar} from 'react-native';
+import {SafeAreaView, ScrollView, View, Text, StatusBar} from 'react-native';
+import {createAppContainer} from 'react-navigation';
+import {createStackNavigator} from 'react-navigation-stack';
+import styles from './appstyle';
 
-const App = () => {
+const LandingPage = () => {
   return (
     <>
       <StatusBar barStyle="dark-content" />
-      <SafeAreaView>\</SafeAreaView>
+      <SafeAreaView>
+        <ScrollView>
+          <View id="container" style={styles.container}>
+            <Text id="appTitle" style={styles.appTitle}>
+              Aim Game
+            </Text>
+            <Text id="hint" style={styles.hint}>
+              Hint : you need to pop all the bubbles appears
+            </Text>
+          </View>
+        </ScrollView>
+      </SafeAreaView>
     </>
   );
 };
+
+const Navigator = createStackNavigator({
+  LandingPage,
+});
+
+const App = createAppContainer(Navigator);
 
 export default App;
---
+++ bubbleHuntNative/appstyle.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import {StyleSheet} from 'react-native';
+
+export default StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+  appTitle: {
+    fontSize: 26,
+    textAlign: 'center',
+  },
+  hint: {
+    fontSize: 16,
+    textAlign: 'center',
+  },
+});
